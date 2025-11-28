e78c504e5dbdaccdc5d85dd30dfd024159a514c2
markjcrane <markjcrane@gmail.com>
2025-11-28 15:10:51 -0700

    Fix two spelling mistakes

--- app_defaults.php
+++ app_defaults.php
@@ -4,8 +4,8 @@ if ($domains_processed == 1) {
 	$ffmpeg_path = shell_exec('which ffmpeg');
 	if (empty($ffmpeg_path)) {
 	    echo "Please install ffmpeg\n";
-	    echo "On Debian / Ubuntu Linux install ffmpeg witht his command.\n";
-	    echo "apt instal ffmpeg\n";
+	    echo "On Debian / Ubuntu Linux install ffmpeg with this command.\n";
+	    echo "apt install ffmpeg\n";
 	}
 }
 
