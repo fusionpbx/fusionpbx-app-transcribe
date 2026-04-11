5230939cb3142a16e64d4dd667d6477e97959bfa
FusionPBX <markjcrane@gmail.com>
2026-04-11 18:07:27 +0000

    Check PHP_SAPI before executing ffmpeg path check

--- app_defaults.php
+++ app_defaults.php
@@ -1,11 +1,13 @@
 <?php
 
 if ($domains_processed == 1) {
-	$ffmpeg_path = shell_exec('which ffmpeg');
-	if (empty($ffmpeg_path)) {
-	    echo "Please install ffmpeg\n";
-	    echo "On Debian / Ubuntu Linux install ffmpeg with this command.\n";
-	    echo "apt install ffmpeg\n";
+	if (PHP_SAPI === 'cli') {
+		$ffmpeg_path = shell_exec('which ffmpeg');
+		if (empty($ffmpeg_path)) {
+			echo "Please install ffmpeg\n";
+			echo "On Debian / Ubuntu Linux install ffmpeg with this command.\n";
+			echo "apt install ffmpeg\n";
+		}
 	}
 }
 
